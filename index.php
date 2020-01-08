485934df99c02dd853ce31e0c3020d278c77378f
jdkfx <amateurranch@gmail.com>
2020-01-08 18:55:38 +0900

    追加：セッション情報を抽出した

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/config.php';
+
+use Goutte\Client;
+
+$cli = getLoggedInClient();
+// $home = $cli->request(
+//     'GET',
+//     'https://hitpo.it-hiroshima.ac.jp/PfStudent/Portal'
+// );
+
+// echo $home->filter('.current_page_item')->first()->text();
+
+
+function getLoggedInClient(){
+    $topUrl = 'https://hitpo.it-hiroshima.ac.jp/PfStudent/Login?target_url=%2fPfStudent%2fPortal';
+    $cli = new Client();
+    $cli->setHeader('User-Agent', "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0");
+    $top = $cli->request('GET', $topUrl);
+    // $cookies = $cli->getCookieJar()->get('__AntiXsrfToken')->getValue();
+    // $cookies = $cli->getCookieJar()->set(new Cookie('__AntiXsrfToken',));
+    // print_r($cookies);
+    // exit;
+    $session = $top->filter('div.aspNetHidden')->each(function($node){
+        $node->filter("input")->each(function($inputform){
+            echo "NAME:" . $inputform->attr("name") . "\n";
+            echo "VALUE:" . $inputform->attr("value") . "\n";
+        });
+    });
+    // $loginForm = $top->filter('form')->form();
+    // $loginForm['ctl00$MainContent$login_id']        = LOGIN_ID;
+    // $loginForm['ctl00$MainContent$login_password']  = LOGIN_PASSWORD;
+    // $cli->submit($loginForm);
+    // return $cli;
+}
